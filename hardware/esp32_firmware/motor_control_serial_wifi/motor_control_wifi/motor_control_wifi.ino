d7f1fd78b86c9580c1ccd3e19de30723e9bc4964
Muhammad Umair Javed <muhammad.umair.javed.mct@gmail.com>
2026-03-20 06:50:13 +0500

    Setup the boilerPlate for motor_control via Wifi

--- hardware/esp32_firmware/motor_control_serial_wifi/motor_control_wifi/motor_control_wifi.ino
+++ hardware/esp32_firmware/motor_control_serial_wifi/motor_control_wifi/motor_control_wifi.ino
@@ -1,9 +1,34 @@
+#include <WiFi.h>
+
+const char* ssid = "M_U_J";
+const char* password = "ros2humble";
+
+void initWiFi () {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("Status: ");
+  Serial.println(WiFi.status());
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (WiFi.status() == WL_CONNECTION_LOST){
+    // Preventive measures to be taken in case of Network Lost
+    initWiFi();
+
+  }
 
 }
